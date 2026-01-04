96200d82200c61d7aba4f0b4345c7a998feb80dc
Manish Patel <patelm.2280@gmail.com>
2026-01-04 11:57:11 +0530

    turbo frames implemented

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,7 +5,7 @@ require "application_system_test_case"
 class QuotesTest < ApplicationSystemTestCase
   
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first # Reference to the first fixture quote
   end
 
   test "Creating a new quote" do
